02b84c9a3433e3d27992abbb3770f0bf6f02a14b
Piotr Leszczynski <piotrkun@gmail.com>
2024-05-01 19:01:07 +0200

    configuring the project

---
+++ src/test/java/com/google/MainTest.java
@@ -0,0 +1,12 @@
+package com.google;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+  @Test
+  void should_always_pass() {
+    assertThat(true).isTrue();
+  }
+}
