f366b82f3a14caa3b6e048a6b08be7870c662ae6
Piotr Leszczynski <piotrkun@gmail.com>
2024-05-13 17:52:20 +0200

    Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.

--- src/main/java/com/google/MinStack.java
+++ src/main/java/com/google/MinStack.java
@@ -5,29 +5,57 @@ import java.util.List;
 
 public class MinStack {
   private final List<Integer> stack;
+  private final List<Integer> minValues;
 
   public MinStack() {
     stack = new ArrayList<>();
+    minValues = new ArrayList<>();
   }
 
-  public void push(int val) {
-    stack.add(val);
+  public void push(int num) {
+    stack.add(num);
+    if (!minValues.isEmpty()) {
+      var first = minValues.getFirst();
+      if (first >= num) {
+        shift(minValues);
+        minValues.set(0, num);
+      }
+    } else {
+      minValues.add(num);
+    }
+  }
+
+  public List<Integer> shift(List<Integer> input) {
+    int current = input.getFirst();
+    int next = input.getFirst();
+    for (int i = 0; i < input.size(); i++) {
+      if (input.size() > i + 1) {
+        next = input.get(i + 1);
+        input.set(i + 1, current);
+        current = next;
+      }
+    }
+    input.add(next);
+    return input;
   }
 
   public void pop() {
-    stack.removeLast();
+    int last = stack.removeLast();
+    minValues.remove(Integer.valueOf(last));
   }
 
-  // gets the top element of the stack.
   int top() {
     return stack.getLast();
   }
 
-  //  retrieves the minimum element in the stack.
   int getMin() {
-    return stack.stream()
-        .mapToInt(Integer::intValue)
-        .min()
-        .orElse(-1);
+    if (!minValues.isEmpty()) {
+      return minValues.getFirst();
+    } else {
+      return stack.stream()
+          .mapToInt(Integer::intValue)
+          .min()
+          .orElse(-1);
+    }
   }
 }
