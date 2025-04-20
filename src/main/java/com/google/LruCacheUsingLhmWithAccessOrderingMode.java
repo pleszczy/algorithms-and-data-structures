d75c287490132f196a39535b84a16ca9c9775bd6
Piotr Leszczynski <piotrkun@gmail.com>
2025-04-20 17:41:49 +0200

    LRU cache using lhm with access ordering mode

--- src/main/java/com/google/LruCacheUsingLhmWithAccessOrderingMode.java
+++ src/main/java/com/google/LruCacheUsingLhmWithAccessOrderingMode.java
@@ -1,41 +1,31 @@
 package com.google;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
-public class LruCacheUsingLhmWithAccessOrderingMode {
+public class LruCacheUsingLhmWithAccessOrderingMode extends LinkedHashMap<String, String> {
   private final int capacity;
-  private final LinkedHashMap<String, String> cache;
 
   public LruCacheUsingLhmWithAccessOrderingMode(int capacity) {
+    super(capacity, 1.0f, true);
     this.capacity = capacity;
-    cache = new LinkedHashMap<>(capacity, 1.0f, true);
   }
 
-  public void add(String key, String value) {
-    if (cache.size() >= capacity) {
-      cache.remove(cache.firstEntry().getKey());
-    }
-    cache.putLast(key, value);
-  }
-
-  public String get(String key) {
-    return cache.get(key);
-  }
-
-  public void print() {
-    System.out.println(cache);
+  @Override
+  protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
+    return size() > capacity;
   }
 
   public static void main(String[] args) {
     var lruCache = new LruCacheUsingLhmWithAccessOrderingMode(3);
-    lruCache.add("1", "1");
-    lruCache.add("2", "2");
-    lruCache.add("3", "3");
-    lruCache.add("4", "4");
-    lruCache.add("5", "5");
-    lruCache.add("6", "6");
+    lruCache.put("1", "1");
+    lruCache.put("2", "2");
+    lruCache.put("3", "3");
+    lruCache.put("4", "4");
+    lruCache.put("5", "5");
+    lruCache.put("6", "6");
     System.out.println(lruCache.get("4"));
-    lruCache.add("7", "7");
-    lruCache.print();
+    lruCache.put("7", "7");
+    System.out.println(lruCache);
   }
 }
