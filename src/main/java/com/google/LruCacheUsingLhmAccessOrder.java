2ad96c44bd39260418bd08b0129d79a50a29291c
Piotr Leszczynski <piotrkun@gmail.com>
2025-04-20 17:53:29 +0200

    Testing LruCacheUsingLhmAccessOrder

--- src/main/java/com/google/LruCacheUsingLhmAccessOrder.java
+++ src/main/java/com/google/LruCacheUsingLhmAccessOrder.java
@@ -15,17 +15,4 @@ public class LruCacheUsingLhmAccessOrder extends LinkedHashMap<String, String> {
   protected boolean removeEldestEntry(Map.Entry<String, String> eldest) {
     return size() > capacity;
   }
-
-  public static void main(String[] args) {
-    var lruCache = new LruCacheUsingLhmAccessOrder(3);
-    lruCache.put("1", "1");
-    lruCache.put("2", "2");
-    lruCache.put("3", "3");
-    lruCache.put("4", "4");
-    lruCache.put("5", "5");
-    lruCache.put("6", "6");
-    System.out.println(lruCache.get("4"));
-    lruCache.put("7", "7");
-    System.out.println(lruCache);
-  }
 }
