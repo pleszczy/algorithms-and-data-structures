d7fd13e33021a16407fb8d7fae344b83ac1749e1
Piotr Leszczynski <piotrkun@gmail.com>
2025-04-28 22:07:26 +0200

    minimum health

---
+++ src/test/java/com/algorithms/MinimumHealthTest.java
@@ -0,0 +1,18 @@
+package com.algorithms;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class MinimumHealthTest {
+  MinimumHealth systemUnderTest = new MinimumHealth();
+
+  @Test
+  void should_return_correct_minimum_health() {
+    var health = systemUnderTest.minimumHealth(List.of(1, 2), List.of(3, 4), 2);
+
+    assertThat(health).isEqualTo(6);
+  }
+
+}
